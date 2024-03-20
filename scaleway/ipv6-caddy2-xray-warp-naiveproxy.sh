60a9a2e90122604ef8173e897c067f53f21be745
U201413497 <33078674+U201413497@users.noreply.github.com>
2024-03-20 19:01:32 +0800

    Update ipv6-caddy2-xray-warp-naiveproxy.sh

--- scaleway/ipv6-caddy2-xray-warp-naiveproxy.sh
+++ scaleway/ipv6-caddy2-xray-warp-naiveproxy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 _INSTALL(){
-  echo -e "nameserver 2a01:4f8:c2c:123f::1\nnameserver 2a00:1098:2c::1\nnameserver 2a01:4f9:c010:3f02::1" > /etc/resolv.conf
   apt install curl wget sudo -y
   echo -n "Enter your domain:"
   read domain
@@ -123,7 +122,6 @@ WantedBy=multi-user.target" > /etc/systemd/system/xray.service
   apt-get -y purge lynx memtester unixodbc python-* odbcinst-*  tcpdump ttf-*
   apt-get autoremove && apt-get clean
   wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh c
-  echo -e "nameserver 2001:4860:4860::8888\nnameserver 2001:4860:4860::8844" > /etc/resolv.conf
   systemctl start caddy.service xray.service
 }
 
