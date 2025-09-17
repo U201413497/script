ed807ba060e1f41946e3f95bf4711cacaba2800e
U201413497 <33078674+U201413497@users.noreply.github.com>
2025-09-17 16:19:40 +0800

    Update ssl.sh

--- backupfiles/ssl.sh
+++ backupfiles/ssl.sh
@@ -8,10 +8,10 @@ _INSTALL(){
   mkdir /root/$domain
   curl https://get.acme.sh | sh
   cd /root/.acme.sh/
-  ./acme.sh --server https://api.buypass.com/acme/directory --register-account --accountemail 'u201413794@gmail.com'
+  ./acme.sh --server https://acme-v02.api.letsencrypt.org/directory --register-account --accountemail 'u201413794@gmail.com'
   export CF_Key="2721a50b7beaa571d9ed25fc63ce3fb31b14c"
   export CF_Email="u201413794@gmail.com"
-  ./acme.sh --server https://api.buypass.com/acme/directory --issue -d $domain --dns dns_cf --force
+  ./acme.sh --server https://acme-v02.api.letsencrypt.org/directory --issue -d $domain --dns dns_cf --force
   sleep 5
   ./acme.sh --install-cert -d $domain  --key-file /root/$domain/private.key --fullchain-file /root/$domain/certificate.crt
   ./acme.sh  --upgrade  --auto-upgrade
